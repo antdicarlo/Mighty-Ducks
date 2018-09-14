08b589ea745354fcfa6360544b60842e4419a7cd
KOoba1 <KOoba1@gmail.com>
2018-09-14 12:19:22 -0400

    added users sql

---
+++ database/CreateUsersTable.sql
@@ -0,0 +1,18 @@
+
+
+-- Create login page
+-- create registration page
+-- implement simple login/registration
+-- create  a server side routes folder and move routes 
+-- test payments with production host
+-- test payment move to next route 
+
+CREATE TABLE users(
+
+Firstname VARCHAR(200),
+Lastname VARCHAR(200),
+Email VARCHAR(300),
+Password varchar(200),
+UserId int AUTO_INCREMENT,
+PRIMARY KEY(UserId)
+);
--- server/db.js
+++ server/db.js
@@ -7,7 +7,7 @@ exports.connect = function() {
    host     : process.env.DB_HOST,
     user     : process.env.DB_USER,
     password : process.env.DB_PASSWORD,
-    database : '',
+    database : 'template',
     debug:false
   });
 }
