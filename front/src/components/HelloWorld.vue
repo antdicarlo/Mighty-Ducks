0dae5589582422085c7d561ada7d6ea0f511fd2e
KOoba1 <KOoba1@gmail.com>
2018-10-06 20:38:49 -0400

    updates

--- front/src/components/HelloWorld.vue
+++ front/src/components/HelloWorld.vue
@@ -1,12 +1,23 @@
 <template>
-  <div class="hello">
+   <a-scene embedded>
+    <a-sky color="#000"></a-sky>
+      <a-entity camera look-controls wasd-controls position="0 1 3" rotation="-15 0 0"></a-entity>
+      <a-box color="blue" opacity="0.75" visible="true"></a-box>
+      <a-marker-camera preset='hiro'></a-marker-camera>    
 
-    <h2>Template</h2>
-	
-  </div>
+  </a-scene>
 </template>
 
 <script>
+import 'aframe'
+
+</script>
+
+<style scoped>
+</style>
+
+<script>
+import 'aframe'
 export default {
 
     name: 'HelloWorld',
